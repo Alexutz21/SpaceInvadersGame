ffe2346b025c50471f2c9fbb8637d1040c871cdd
Alexandru Constantin Lungu <alex.lungu21@yahoo.com>
2022-10-25 17:15:03 +0300

    Enemy Wave Config Start
    was created new script waveconfigso.cs

---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject
+{
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] float moveSpeed = 5f;
+
+    public Transform GetStartingWaypoint()
+    {
+        return pathPrefab.GetChild(0);
+    }
+    public List<Transform> GetWaypoint()
+    {
+        List<Transform> waypoint = new List<Transform>();
+        foreach (Transform child in pathPrefab)
+        {
+            waypoint.Add(child);
+        }
+        return waypoint;
+    }
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+}
