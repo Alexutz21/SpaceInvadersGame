05abbd539c2a20a00354f50fdee0950ae6e383ee
Alexandru Constantin Lungu <alex.lungu21@yahoo.com>
2022-10-27 13:38:42 +0300

    Enemy Spawner
    Creating new script EnemySpawner and Create Empty Object into Hierarchy and attach EnemySpawner script and also atach wave
    Into Wave we can select Enemy Prefabs and we can select how many we want.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    //[SerializeField] WaveConfigSO currentWave;
+    [SerializeField] WaveConfigSO currentWave;
 
-    // void Start()
-    // {
-    //     SpawnEmenies();
-    // }
-    // public WaveConfigSO GetCurrentWave()
-    // {
-    //     return currentWave;
-    // }
-    // void SpawnEmenies()
-    // {
-    //     for (int i = 0; i < currentWave.GetEnemyCount(); i++)
-    //     {
-    //         Instantiate(currentWave.GetEnemyPrefab(i),
-    //                currentWave.GetStartingWaypoint().position,
-    //                Quaternion.identity,
-    //                transform);
-    //     }
-
-    // }
+    void Start()
+    {
+        SpawnEmenies();
+    }
+    public WaveConfigSO GetCurrentWave()
+    {
+        return currentWave;
+    }
+    void SpawnEmenies()
+    {
+        for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+        {
+            Instantiate(currentWave.GetEnemyPrefab(i),
+                   currentWave.GetStartingWaypoint().position,
+                   Quaternion.identity,
+                   transform);
+        }
+    }
 }
