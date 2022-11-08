3cf4372557ddcbf38d9c16bacf8a532d55a9eec4
Alexandru Constantin Lungu <alex.lungu21@yahoo.com>
2022-11-08 15:17:35 +0200

    LevelManager
    adding levelManager to our game so we can load the levels

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -14,12 +14,14 @@ public class Health : MonoBehaviour
 
     AudioPlayer audioPlayer;
     ScoreKeeper scoreKeeper;
+    LevelManager levelManager;
 
     void Awake()
     {
         cameraShake = Camera.main.GetComponent<CameraShake>();
         audioPlayer = FindObjectOfType<AudioPlayer>();
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        levelManager = FindObjectOfType<LevelManager>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -55,7 +57,12 @@ public class Health : MonoBehaviour
         {
             scoreKeeper.ModifyScore(score);
         }
+        else
+        {
+            levelManager.LoadGameOver();
+        }
         Destroy(gameObject);
+
     }
     void PlayHitEffect()
     {
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] float sceneLoadDelay = 2f;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad("GameOver", sceneLoadDelay));
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+    IEnumerator WaitAndLoad(string sceneName, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
+}
